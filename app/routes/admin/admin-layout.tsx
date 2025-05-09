10237f2e6ff18c3bd473d2a16ab31dde40efcb27
Keaton Nordby <keatonnordby4@gmail.com>
2025-05-09 15:19:43 -0500

    UI dashboard w hardcoded data

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Outlet } from 'react-router';
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations"
+import { MobileSideBar, NavItems } from 'components';
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
-            MobileSidebar 
+            <MobileSideBar /> 
 
             <aside className="w-full max-w-[270px] hidden lg:block">
-                Sidebar</aside>
+              <SidebarComponent width={270} enableGestures={false}>
+                <NavItems />
+
+              </SidebarComponent>
+
+            </aside>
                 <aside className='children'>
                   <Outlet />
                 </aside>
