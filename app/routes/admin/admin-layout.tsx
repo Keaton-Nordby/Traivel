ec061c652e7fdc872e10cb2acd9f0f17c6783b0b
Keaton Nordby <keatonnordby4@gmail.com>
2025-05-12 20:53:39 -0500

    sign in working properly and admin user assignmed

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,26 +1,44 @@
-import React from 'react'
-import { Outlet } from 'react-router';
-import { SidebarComponent } from "@syncfusion/ej2-react-navigations"
-import { MobileSideBar, NavItems } from 'components';
 
-const AdminLayout = () => {
-  return (
-    <div className="admin-layout">
-            <MobileSideBar /> 
+import {Outlet, redirect} from "react-router";
+import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
+import {MobileSideBar, NavItems} from "../../../components";
+import {account} from "~/appwrite/client";
+import {getExistingUser, storeUserData} from "~/appwrite/auth";
 
-            <aside className="w-full max-w-[270px] hidden lg:block">
-              <SidebarComponent width={270} enableGestures={false}>
-                <NavItems />
+export async function clientLoader() {
+    try {
+        const user = await account.get();
 
-              </SidebarComponent>
+        if(!user.$id) return redirect('/sign-in');
 
-            </aside>
-                <aside className='children'>
-                  <Outlet />
-                </aside>
-    </div>
-    
-  )
+        const existingUser = await getExistingUser(user.$id);
+
+        if(existingUser?.status === 'user') {
+            return redirect('/');
+        }
+
+        return existingUser?.$id ? existingUser : await storeUserData();
+    } catch (e) {
+        console.log('Error in clientLoader', e)
+        return redirect('/sign-in')
+    }
 }
 
-export default AdminLayout;
+const AdminLayout = () => {
+    return (
+        <div className="admin-layout">
+            <MobileSideBar />
+
+            <aside className="w-full max-w-[270px] hidden lg:block">
+                <SidebarComponent width={270} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
+            </aside>
+
+            <aside className="children">
+                <Outlet />
+            </aside>
+        </div>
+    )
+}
+export default AdminLayout
